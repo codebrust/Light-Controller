72607f6c43e80e48005763b1d8336339079411e4
Suresh Chaudhari <dangernose@gmail.com>
2019-04-29 08:04:56 +0545

    Init

---
+++ app/src/main/java/com/suresh/lightcontroller/MQTTClient.java
@@ -0,0 +1,50 @@
+package com.suresh.lightcontroller;
+
+import android.util.Log;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class MQTTClient {
+
+    private static final String TAG = "MQTTClient";
+    private String mqttBroker = "tcp://iot.eclipse.org:1883";
+    private String mqttTopic = "codifythings/lightcontrl";
+    private String deviceId = "androidClient";
+    private String messageContent = "SWITCH";
+
+    public void publishToMQTT() throws MqttException {
+        // Request clean session in the connection options.
+        Log.i(TAG, "Setting Connection Options");
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setCleanSession(true);
+
+
+
+        // Attempt a connection to MQTT broker using the values
+        // of connection variables.
+        Log.i(TAG, "Creating New Client");
+        MqttClient client = new MqttClient(mqttBroker, deviceId,
+                new MemoryPersistence());
+        client.connect(options);
+
+
+
+        // Publish message to topic
+        Log.i(TAG, "Publishing to Topic");
+        MqttMessage mqttMessage =
+                new MqttMessage(messageContent.getBytes());
+        mqttMessage.setQos(2);
+        client.publish(mqttTopic, mqttMessage);
+        Log.i(TAG, "Publishing Complete");
+
+
+
+        Log.i(TAG, "Disconnecting from MQTT");
+        client.disconnect();
+    }
+
+}
---
+++ app/src/main/java/com/suresh/lightcontroller/MainActivity.java
@@ -0,0 +1,46 @@
+package com.suresh.lightcontroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView lightIcon = (ImageView) findViewById(R.id.light_icon);
+        lightIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO - add action
+
+                try{
+
+                }catch (Exception ex){
+                    Log.e(TAG,ex.getMessage());
+                }
+
+            }
+        });
+        
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
